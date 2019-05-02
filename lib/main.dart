f50e325638d9e56becda1331e0f50f0b5c348a4e
rg16 <indrian16@outlook.co.id>
2019-05-02 21:45:31 +0800

    init project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:login_firebase/login/login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Login',
+      home: LoginPage(),
+    );
+  }
+}
